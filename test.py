2b575af4f908ed32265b9fefc89982257852ae4d
remsteele <rsteele2215@sdsu.edu>
2024-04-09 14:51:44 -0700

    Added api testing notebook

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-print("Hello World!")
-for i in range(10):
-    print("I love AI")
 
-print("a")
+l1, l2, carry = 7, 4, 2
+
+value = l1 + l2 + carry
+print(value)
+value, carry = value % 10, value // 10
+
+print(value, carry)
